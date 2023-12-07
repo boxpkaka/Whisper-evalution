edf9f227ec17d5f00f472533dc713ab7f6cb9ebd
yumingdong <1016097967@qq.com>
2023-12-07 03:27:49 +0000

    添加每步上下文管理与总测试上下文管理

--- eval/eval_whisper.py
+++ eval/eval_whisper.py
@@ -3,9 +3,7 @@ import torch
 import whisper
 import os
 import pynvml
-import psutil
 from tqdm import tqdm
-from typing import List
 from transformers import WhisperProcessor
 from faster_whisper import WhisperModel
 from dataloader import get_dataloader
@@ -13,7 +11,7 @@ from eval.eval_with_trn import eval_with_trn
 from norm.norm_with_trn import normalize_cantonese
 from utils.count_model import count_model
 from utils.get_save_file import save_file
-from utils.count_usage import CountTime
+from utils.count_usage import StepCounter, TrainMonitor
 from utils.get_audio_duration import get_duration_from_idx
 from utils.get_model import get_pipeline, load_whisper
 
@@ -26,21 +24,6 @@ def save_eval(export_dir, refs, trans, trans_with_time=None):
         save_file(os.path.join(export_dir, 'reg_rtf.trn'), trans_with_time)
     normalize_cantonese(export_dir)
     eval_with_trn(export_dir)
-
-
-def get_usage_info(total_cost_time: float, total_audio_time: float,
-                   memory: List, max_cpu_usage: float,
-                   trans_with_info: List) -> None:
-    rtf = round(total_cost_time / total_audio_time, 3)
-    memory_max = max(memory)
-    memory_avg = sum(memory) / len(memory)
-
-    trans_with_info.append(f'total cost time: {total_cost_time}s')
-    trans_with_info.append(f'RTF:             {rtf}')
-    trans_with_info.append(f'Throughput:      {round(1/rtf, 3)}')
-    trans_with_info.append(f'Avg memory:      {memory_avg}')
-    trans_with_info.append(f'Max memory:      {memory_max}')
-    trans_with_info.append(f'Max cpu usage:   {max_cpu_usage}')
 
 
 def eval_whisper_openai(model_path: str, dataset_dir: str, export_dir: str, language: str, device: torch.device):
@@ -79,60 +62,47 @@ def eval_faster_whisper(model_path: str, dataset_dir: str, export_dir: str, lang
             compute_type = 'float16'
 
     model = WhisperModel(model_path, device=device, compute_type=compute_type,
-                         device_index=device_index, num_workers=num_workers,
-                         )
+                         device_index=device_index, num_workers=num_workers)
 
     if 'large-v3' in model_path:
         model.feature_extractor.mel_filters = \
             model.feature_extractor.get_mel_filters(model.feature_extractor.sampling_rate,
                                                     model.feature_extractor.n_fft, n_mels=128)
 
-    dataloader = get_dataloader(dataset_dir, None, batch_size=1, shuffle=False, type='whisper_faster')
+    dataloader = get_dataloader(dataset_dir, None, batch_size=32, shuffle=False, type='whisper_faster')
 
     pynvml.nvmlInit()
     handle = pynvml.nvmlDeviceGetHandleByIndex(device_index)
 
-    refs = []
-    trans = []
-    memory = []
-    trans_with_info = []
+    with TrainMonitor() as monitor:
+        for batch in tqdm(dataloader):
+            data_path, ref, idx = batch
+            data_path = data_path[0]
+            ref = ref[0]
+            idx = idx[0]
+            with StepCounter(handle) as ct:
+                wav, _ = soundfile.read(data_path)
+                segments, info = model.transcribe(audio=data_path, language=language)
+                for segment in segments:
+                    transcription = segment.text
 
-    total_cost_time = 0
-    total_audio_time = 0
-    max_cpu_usage = 0
+            cost_time = ct.cost_time
+            memory_used = ct.cost_memory
+            cpu_usage = ct.cpu_usage
 
-    for batch in tqdm(dataloader):
-        data_path, ref, idx = batch
-        data_path = data_path[0]
-        ref = ref[0]
-        idx = idx[0]
-        with CountTime(handle) as ct:
-            wav, _ = soundfile.read(data_path)
-            print(wav.shape)
-            segments, info = model.transcribe(audio=data_path, language=language)
-            for segment in segments:
-                transcription = segment.text
-        print(transcription)
-        cost_time = ct.cost_time
-        memory_used = ct.cost_memory
+            monitor.total_cost_time += cost_time
+            monitor.memory.append(memory_used)
+            monitor.max_cpu_usage = max(cpu_usage, monitor.max_cpu_usage)
+            monitor.total_audio_time += get_duration_from_idx(idx)
+            monitor.refs.append(f'{ref} ({idx})')
+            monitor.trans.append(f'{transcription} ({idx})')
+            monitor.trans_with_info.append(f'{transcription} ({idx})'
+                                           f'batch-info: cost time: {cost_time} '
+                                           f'used memory: {memory_used} '
+                                           f'cpu usage: {cpu_usage}')
 
-        total_cost_time += cost_time
-        memory.append(memory_used)
-
-        cpu_usage = psutil.cpu_percent(interval=1)
-        max_cpu_usage = max(cpu_usage, max_cpu_usage)
-
-
-        refs.append(f'{ref} ({idx})')
-        trans.append(f'{transcription} ({idx})')
-        trans_with_info.append(f'{transcription} ({idx})'
-                               f'batch-info: cost time: {cost_time} '
-                               f'used memory: {memory_used} '
-                               f'cpu usage: {cpu_usage}')
-
-    get_usage_info(total_cost_time, total_audio_time, memory, max_cpu_usage, trans_with_info)
     export_dir += f'-{device}-{compute_type}'
-    save_eval(export_dir, refs, trans)
+    save_eval(export_dir, monitor.refs, monitor.trans, monitor.trans_with_info)
 
 
 def eval_whisper_huggingface(model_path: str, dataset_dir: str, export_dir: str,
@@ -145,51 +115,41 @@ def eval_whisper_huggingface(model_path: str, dataset_dir: str, export_dir: str,
     pynvml.nvmlInit()
     handle = pynvml.nvmlDeviceGetHandleByIndex(device.index)
 
-    refs = []
-    trans = []
-    memory = []
-    trans_with_info = []
-
-    total_cost_time = 0
-    total_audio_time = 0
-    max_cpu_usage = 0
-
     model.to(device)
     model.eval()
-    with torch.cuda.amp.autocast():
-        with torch.no_grad():
-            for batch in tqdm(dataloader):
+    with TrainMonitor() as monitor:
+        with torch.cuda.amp.autocast():
+            with torch.no_grad():
+                for batch in tqdm(dataloader):
 
-                with CountTime(handle) as ct:
-                    input_features, ref, idx = batch
-                    input_features = input_features.to(device)
+                    with StepCounter(handle) as ct:
+                        input_features, ref, idx = batch
+                        input_features = input_features.to(device)
 
-                    predicted_ids = model.generate(input_features, task='transcribe', language=language)
-                    transcription = processor.batch_decode(predicted_ids, skip_special_tokens=True)
+                        predicted_ids = model.generate(input_features, task='transcribe', language=language)
+                        transcription = processor.batch_decode(predicted_ids, skip_special_tokens=True)
 
-                cost_time = ct.cost_time
-                memory_used = ct.cost_memory
+                    cost_time = ct.cost_time
+                    memory_used = ct.cost_memory
+                    cpu_usage = ct.cpu_usage
 
-                total_cost_time += cost_time
-                memory.append(memory_used)
+                    monitor.total_cost_time += cost_time
+                    monitor.memory.append(memory_used)
+                    monitor.max_cpu_usage = max(cpu_usage, monitor.max_cpu_usage)
 
-                cpu_usage = psutil.cpu_percent(interval=1)
-                max_cpu_usage = max(cpu_usage, max_cpu_usage)
+                    for i in range(len(transcription)):
+                        monitor.total_audio_time += get_duration_from_idx(idx[i])
+                        monitor.refs.append(f'{ref[i]} ({idx[i]})')
+                        monitor.trans.append(f'{transcription[i]} ({idx[i]})')
+                        if i == 0:
+                            monitor.trans_with_info.append(f'batch-info: cost time: {cost_time} '
+                                                           f'used memory: {memory_used} '
+                                                           f'cpu usage: {cpu_usage}')
+                            monitor.trans_with_info.append(f'{transcription[i]} ({idx[i]}) ')
+                        else:
+                            monitor.trans_with_info.append(f'{transcription[i]} ({idx[i]})')
 
-                for i in range(len(transcription)):
-                    total_audio_time += get_duration_from_idx(idx[i])
-                    refs.append(f'{ref[i]} ({idx[i]})')
-                    trans.append(f'{transcription[i]} ({idx[i]})')
-                    if i == 0:
-                        trans_with_info.append(f'batch-info: cost time: {cost_time} '
-                                               f'used memory: {memory_used} '
-                                               f'cpu usage: {cpu_usage}')
-                        trans_with_info.append(f'{transcription[i]} ({idx[i]}) ')
-                    else:
-                        trans_with_info.append(f'{transcription[i]} ({idx[i]})')
-
-    get_usage_info(total_cost_time, total_audio_time, memory, max_cpu_usage, trans_with_info)
-    save_eval(export_dir, refs, trans, trans_with_info)
+    save_eval(export_dir, monitor.refs, monitor.trans, monitor.trans_with_info)
 
 
 def eval_whisper_pipeline(model_path: str, dataset_dir: str, export_dir: str,
@@ -204,45 +164,34 @@ def eval_whisper_pipeline(model_path: str, dataset_dir: str, export_dir: str,
     pynvml.nvmlInit()
     handle = pynvml.nvmlDeviceGetHandleByIndex(device.index)
 
-    refs = []
-    trans = []
-    memory = []
-    trans_with_info = []
+    with TrainMonitor as monitor:
+        for batch in tqdm(dataloader):
+            data_path, ref, idx = batch
+            data_path = list(data_path)
 
-    total_cost_time = 0
-    total_audio_time = 0
-    max_cpu_usage = 0
+            with StepCounter(handle) as ct:
+                result = pipe(data_path, return_timestamps=False, generate_kwargs=generate_kwargs)
 
-    for batch in tqdm(dataloader):
-        data_path, ref, idx = batch
-        data_path = list(data_path)
+            cost_time = ct.cost_time
+            memory_used = ct.cost_memory
+            cpu_usage = ct.cpu_usage
 
-        with CountTime(handle) as ct:
-            result = pipe(data_path, return_timestamps=False, generate_kwargs=generate_kwargs)
+            monitor.total_cost_time += cost_time
+            monitor.memory.append(memory_used)
+            monitor.max_cpu_usage = max(cpu_usage, monitor.max_cpu_usage)
 
-        cost_time = ct.cost_time
-        memory_used = ct.cost_memory
-        print(cost_time)
-
-        total_cost_time += cost_time
-        memory.append(memory_used)
-
-        cpu_usage = psutil.cpu_percent(interval=1)
-        max_cpu_usage = max(cpu_usage, max_cpu_usage)
-
-        for i in range(len(result)):
-            transcription = result[i]['text']
-            total_audio_time += get_duration_from_idx(idx[i])
-            refs.append(f'{ref[i]} ({idx[i]})')
-            trans.append(f'{transcription} ({idx[i]})')
-            if i == 0:
-                trans_with_info.append(f'batch-info: cost time: {cost_time} '
-                                       f'used memory: {memory_used} '
-                                       f'cpu usage: {cpu_usage}')
-                trans_with_info.append(f'{transcription} ({idx[i]}) ')
-            else:
-                trans_with_info.append(f'{transcription} ({idx[i]})')
+            for i in range(len(result)):
+                transcription = result[i]['text']
+                monitor.total_audio_time += get_duration_from_idx(idx[i])
+                monitor.refs.append(f'{ref[i]} ({idx[i]})')
+                monitor.trans.append(f'{transcription} ({idx[i]})')
+                if i == 0:
+                    monitor.trans_with_info.append(f'batch-info: cost time: {cost_time} '
+                                           f'used memory: {memory_used} '
+                                           f'cpu usage: {cpu_usage}')
+                    monitor.trans_with_info.append(f'{transcription} ({idx[i]}) ')
+                else:
+                    monitor.trans_with_info.append(f'{transcription} ({idx[i]})')
 
     export_dir += f'-pipeline'
-    get_usage_info(total_cost_time, total_audio_time, memory, max_cpu_usage, trans_with_info)
-    save_eval(export_dir, refs, trans, trans_with_info)
+    save_eval(export_dir, monitor.refs, monitor.trans, monitor.trans_with_info)
--- utils/count_usage.py
+++ utils/count_usage.py
@@ -1,13 +1,15 @@
 import time
 import pynvml
+import psutil
 
 
-class CountTime(object):
+class StepCounter(object):
     def __init__(self, handle=None):
         self.time = 0
         self.cost_time = 0
         self.cost_memory = 0
         self.handle = handle
+        self.cpu_usage = 0
 
     def __enter__(self):
         self.time = time.perf_counter()
@@ -15,6 +17,7 @@ class CountTime(object):
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         cost_time = round(time.perf_counter() - self.time, 3)
+        self.cpu_usage = psutil.cpu_percent(interval=1)
         if self.handle is not None:
             memory_info = pynvml.nvmlDeviceGetMemoryInfo(self.handle)
             self.cost_memory = memory_info.used / 1024 ** 2
@@ -22,22 +25,31 @@ class CountTime(object):
 
 
 class TrainMonitor:
-    def __init__(self, handle=None):
-        self.time = 0
-        self.cost_time = 0
-        self.cost_memory = 0
-        self.handle = handle
+    def __init__(self):
+        self.refs = []
+        self.trans = []
+        self.memory = []
+        self.trans_with_info = []
+
+        self.total_cost_time = 0
+        self.total_audio_time = 0
+        self.max_cpu_usage = 0
 
     def __enter__(self):
-        self.time = time.perf_counter()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        cost_time = round(time.perf_counter() - self.time, 3)
-        if self.handle is not None:
-            memory_info = pynvml.nvmlDeviceGetMemoryInfo(self.handle)
-            self.cost_memory = memory_info.used / 1024 ** 2
-        self.cost_time = cost_time
+        rtf = round(self.total_cost_time / self.total_audio_time, 3)
+        memory_max = max(self.memory)
+        memory_avg = sum(self.memory) / len(self.memory)
+
+        self.trans_with_info.append(f'total cost time: {self.total_cost_time}s')
+        self.trans_with_info.append(f'RTF:             {rtf}')
+        self.trans_with_info.append(f'Throughput:      {round(1 / rtf, 3)}')
+        self.trans_with_info.append(f'Avg memory:      {memory_avg}')
+        self.trans_with_info.append(f'Max memory:      {memory_max}')
+        self.trans_with_info.append(f'Max cpu usage:   {self.max_cpu_usage}')
+
 
 if __name__ == '__main__':
     pass
